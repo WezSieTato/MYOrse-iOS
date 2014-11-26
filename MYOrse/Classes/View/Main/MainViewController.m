5ecd9a2b7b8d18089efe2df14f7b704bff30c513
WezSieTato <le0n.uo@gmail.com>
2014-11-26 11:29:33 +0100

    Move message texts to localizable strings

--- MYOrse/Classes/View/Main/MainViewController.m
+++ MYOrse/Classes/View/Main/MainViewController.m
@@ -34,7 +34,6 @@
 
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
-    [self.navigationController setToolbarHidden:NO animated:animated];
 }
 
 -(void)viewDidDisappear:(BOOL)animated{
@@ -50,8 +49,8 @@
 - (IBAction)startStopReceiver:(UIBarButtonItem *)sender {
     _started = !_started;
     
-    sender.title = _started ? @"Zatrzymaj odbiornik" : @"Uruchom odbiornik";
-    NSString* siemaMsg = _started ? @"Twoj rozmowca korzysta teraz z programu MYOrse, będzie odbieral twoje wiadomości w alfabecie Morse'a, dlatego staraj się pisać krótkie i zwięzłe wiadomości, gdy twój rozmówca odczyta wiadomość dostaniesz wiadomość zwrotną!" : @"Twój rozmówca rozłączył się z MYOrse!";
+    sender.title = NSLocalizedString( _started ? @"STOP_MYORSE_SERVICE" : @"START_MYORSE_SERVICE", nil);
+    NSString* siemaMsg = NSLocalizedString( !_started ? @"STOP_MYORSE_MESSAGE" : @"START_MYORSE_MESSAGE", nil);
     _btnPick.enabled = !_started;
     
     [[GTalkConnection sharedInstance] sendMessageTo:_pickedFriend withBody:siemaMsg];
@@ -69,6 +68,7 @@
     _lblOtherUser.text = email ? : @"";
     _pickedFriend = email ? : @"";
     _btnStart.enabled = email != nil;
+    [self.navigationController setToolbarHidden:email == nil animated:YES];
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
